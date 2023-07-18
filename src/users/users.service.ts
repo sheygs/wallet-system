c92c5a4eecca737253e8ddf369908116e4872e7c
Olusegun Ekoh <sheygs@Oluseguns-MacBook-Pro.local>
2023-07-18 08:24:35 +0100

    Remove unused import

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
